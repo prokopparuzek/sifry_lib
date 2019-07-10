f96cdf2867fd62c3e09e554a1fd03ee6095f190e
prokopparuzek <prokop.paruzek@paruzkovi.cz>
2019-07-10 14:05:31 +0200

    0.2 obdelnik

--- crypt/transpozice.go
+++ crypt/transpozice.go
@@ -1,22 +1,22 @@
-package transpozice
+package crypt
 
 import (
 	"strings"
 )
 
 type Rectangle struct {
-	weight uint64
-	height uint64
+	Weight uint64
+	Height uint64
 }
 
-func (r *Rectangle) crypt(plain *string) (crypt string) {
+func (r *Rectangle) CryptL(plain *string) (crypt string) {
 	str := strings.Replace(*plain, "\n", "", -1)
-	for i := uint64(0); i < r.height; i++ {
-		for j := uint64(0); j < r.weight; j++ {
-			if i*r.weight+j < uint64(len(str)) {
-				crypt += string([]rune(str)[i*r.weight+j])
+	for i := uint64(0); i < r.Height; i++ {
+		for j := uint64(0); j < r.Weight; j++ {
+			if i*r.Weight+j < uint64(len(str)) {
+				crypt += string([]rune(str)[i*r.Weight+j])
 			} else {
-				crypt += "x"
+				crypt += "X"
 			}
 		}
 		crypt += "\n"
@@ -24,7 +24,33 @@ func (r *Rectangle) crypt(plain *string) (crypt string) {
 	return
 }
 
-func (r *Rectangle) Decrypt(crypt *string) (plain string) {
+func (r *Rectangle) DecryptL(crypt *string) (plain string) {
 	plain = strings.Replace(*crypt, "\n", "", -1)
+	return
+}
+
+func (r *Rectangle) CryptR(plain *string) (crypt string) {
+	str := []rune(*plain)
+	for i := uint64(0); i < r.Height; i++ {
+		for j := uint64(0); j < r.Weight; j++ {
+			if j*r.Height+i < uint64(len(str)) {
+				crypt += string(str[j*r.Height+i])
+			} else {
+				crypt += "X"
+			}
+		}
+		crypt += "\n"
+	}
+	return
+}
+
+func (r *Rectangle) DecryptR(crypt *string) (plain string) {
+	raw := strings.Replace(*crypt, "\n", "", -1)
+	str := []rune(raw)
+	for i := uint64(0); i < r.Height; i++ {
+		for j := uint64(0); j < r.Weight; j++ {
+			plain += string(str[j*r.Height+i])
+		}
+	}
 	return
 }
