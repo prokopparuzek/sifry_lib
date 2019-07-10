4fff5a8fec88ec044f720e66a836928eb8202bfd
prokopparuzek <prokop.paruzek@paruzkovi.cz>
2019-07-10 15:06:04 +0200

    0.1.2.1 reverze

--- crypt/transpozice.go
+++ crypt/transpozice.go
@@ -45,12 +45,19 @@ func (r *Rectangle) CryptR(plain *string) (crypt string) {
 }
 
 func (r *Rectangle) DecryptR(crypt *string) (plain string) {
-	raw := strings.Replace(*crypt, "\n", "", -1)
-	str := []rune(raw)
+	str := []rune(strings.Replace(*crypt, "\n", "", -1))
 	for i := uint64(0); i < r.Height; i++ {
 		for j := uint64(0); j < r.Weight; j++ {
 			plain += string(str[j*r.Height+i])
 		}
+	}
+	return
+}
+
+func Reverse(in *string) (out string) {
+	str := []rune(*in)
+	for i := len(str) - 1; i >= 0; i-- {
+		out += string(str[i])
 	}
 	return
 }
